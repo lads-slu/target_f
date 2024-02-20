0bd7b33f5cfd6e7ecbd78d283c16a9fe99dc1d40
Kristin Persson <kristin.persson@slu.se>
2024-02-20 08:17:00 +0100

    reorganized files

--- scripts/backup_20230405/hampel.R
+++
@@ -1,5 +0,0 @@
-low <- median(a) - 3 * mad(a, constant = 1) #hampel filter https://statsandr.com/blog/outliers-detection-in-r/#hampel-filter
-high <- median(a) + 3 * mad(a, constant = 1)
-
-outlier_ind <- which(a < lower_bound | a > upper_bound)
-outlier_ind
